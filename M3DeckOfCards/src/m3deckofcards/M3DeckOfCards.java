a7d923ca346c2c3a01512e3eea7b8e9da8d230c2
sfcsumb <97874073+sfcsumb@users.noreply.github.com>
2022-03-15 09:46:37 -0700

    Add files via upload

---
+++ M3DeckOfCards/src/m3deckofcards/M3DeckOfCards.java
@@ -0,0 +1,28 @@
+//  M3DeckOfCards.java : M3 Deck Of Cards assignment
+//  Create a slot machine
+//
+//  Steven Forgacs - 15 Mar 2022
+//  CST-338
+//
+//	Version:	1
+//
+//	15 Mar 2022	slf     Initial
+
+package m3deckofcards;
+
+/**
+ *
+ * @author Steve
+ */
+public class M3DeckOfCards
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
